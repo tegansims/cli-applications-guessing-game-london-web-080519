1c50cb3d8388a4be9dde0cd005022d6c2a126331
Tegan Sims <tegansims@hotmail.com>
2019-07-23 12:20:55 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,13 +3,13 @@
 def run_guessing_game
   computer_turn = 1 + rand(6) # Generate and store a random number between 1 and 6
   puts "Give me a number between 1 and 6"# Prompts the user to guess their own number between 1 and 6
-  your_turn = gets.chomp.to_i # Capture user input from the command line
+  your_turn = gets.chomp # Capture user input from the command line
 # Compare that input to the random number that has been generated
 # Print out one of three statements:
 # If the user's input matches the random number: You guessed the correct number!
 # If the user's input DOES NOT matches the random number: Sorry! The computer guessed <number>.
 # It the user's input is equal to "exit": Goodbye!
-  if computer_turn == your_turn
+  if computer_turn == your_turn.to_i
     puts "You guessed the right number!"
   elsif your_turn == "exit"
     puts "Goodbye!"
